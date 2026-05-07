58a30f61a7291bf5e7f7e2bea4d3ab9bc562a7b8
uyy-create <davilucho2002@hotmail.com>
2026-05-07 20:11:11 +0200

    Fixes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  IonCard,
-  IonCardContent,
-  IonCol,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
   IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
   IonGrid,
-  IonProgressBar,
   IonRow,
+  IonCol,
+  IonProgressBar,
+  IonButton,
+  IonIcon,
+  ModalController,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 import { TaskService } from '../services/task.service';
+import { AddTaskModalComponent } from '../components/add-task-modal/add-task-modal.component';
 
 @Component({
   selector: 'app-tab1',
@@ -18,21 +29,52 @@ import { TaskService } from '../services/task.service';
   standalone: true,
   imports: [
     CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
     IonContent,
     IonCard,
+    IonCardHeader,
+    IonCardTitle,
     IonCardContent,
     IonGrid,
     IonRow,
     IonCol,
     IonProgressBar,
+    IonButton,
+    IonIcon,
   ],
 })
 export class Tab1Page {
   stats = { total: 0, completed: 0, pending: 0 };
 
-  constructor(private taskService: TaskService) {}
+  constructor(
+    private taskService: TaskService,
+    private modalCtrl: ModalController
+  ) {
+    addIcons({ add });
+  }
 
   ionViewWillEnter() {
     this.stats = this.taskService.getStats();
+  }
+
+  async openAddTaskModal() {
+    const modal = await this.modalCtrl.create({
+      component: AddTaskModalComponent,
+    });
+    await modal.present();
+
+    const { data } = await modal.onDidDismiss();
+    if (data) {
+      this.taskService.addTask({
+        title: data.title,
+        description: data.description,
+        priority: data.priority,
+        category: data.category,
+        completed: false,
+      });
+      this.stats = this.taskService.getStats();
+    }
   }
 }
