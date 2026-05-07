19f98d2d85079696769411f8b55075c624cd5652
uyy-create <davilucho2002@hotmail.com>
2026-05-07 19:05:04 +0200

    Tareas2

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonAvatar,
+  IonBadge,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardContent,
+  IonChip,
+  IonLabel,
+  IonList,
+  IonItem,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { school, calendar } from 'ionicons/icons';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonAvatar,
+    IonBadge,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardContent,
+    IonChip,
+    IonLabel,
+    IonList,
+    IonItem,
+    IonIcon,
+    ExploreContainerComponent,
+  ],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor() {
+    addIcons({ school, calendar });
+  }
 }
